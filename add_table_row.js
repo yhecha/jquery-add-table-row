0544f6e78384d67e9d36c04e2bcff9d12b27c436
Yasuaki Yano <he.yasu00@gmail.com>
2018-10-01 21:33:57 +0900

    追加する行数の上限をセットできるようにしたっス。

--- add_table_row.js
+++ add_table_row.js
@@ -1,4 +1,7 @@
 $(function() {
+    // テーブルのtbodyの最大行数を設定
+    var maxRows = 10;
+
     // 行が1行しかなかったら、行の削除ボタンを表示させない(入力行が無くなってしまうので)
     if (hideDeleteBtn("#tbl tbody")) {
         $('.btn_deleteTableRow').hide();
@@ -12,6 +15,10 @@ $(function() {
         $("#tbl tbody tr:last-child input").val("");
         // 削除ボタンを表示する
         $('.btn_deleteTableRow').show();
+        // 行が上限まで到達していた場合は行追加ボタンをDisabledにする
+        if (isMaxRow("#tbl tbody", maxRows)) {
+            $('#btn_addTableRow').prop("disabled", true);
+        }
     });
 
     // 削除ボタンが押された行を削除する
@@ -21,6 +28,10 @@ $(function() {
         // 削除ボタンが押された時にも削除ボタン非表示判定を行う。
         if (hideDeleteBtn("#tbl tbody")) {
             $('.btn_deleteTableRow').hide();
+        }
+        // 行追加ボタンのDisabledを解除する
+        if (!isMaxRow("#tbl tbody", maxRows)) {
+            $('#btn_addTableRow').prop("disabled", false);
         }
     });
 });
@@ -37,6 +48,22 @@ function hideDeleteBtn(elem)
         if (len == 1) {
             return true;
         }
+    }
+    return false;
+}
+
+/** 行数上限チェック
+ *  指定した行数以上かどうかをチェックする
+ * 
+ * element elm    カウントしたいテーブルのtbodyの要素
+ * int     maxRow 指定する最大行数
+ * return bool true: 上限, false: 上限ではない
+ */
+function isMaxRow(elem, maxRow)
+{
+    var len = $(elem).children().length;
+    if (len >= maxRow) {
+        return true;
     }
     return false;
 }
